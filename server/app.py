8edadd2a195bfd5069cfaff16926348fdfcb2185
yassinyussuf <yassinyussuf414@gmail.com>
2024-07-01 15:23:50 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
@@ -23,6 +24,52 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return [restaurant.to_dict(rules=['-restaurant_pizzas']) for restaurant in restaurants], 200
+
+@app.route('/restaurants/<int:id>', methods=['GET', 'DELETE'])
+def get_restaurant_by_id(id):
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+
+    if request.method == 'GET':
+        if not restaurant:
+            return {'error': 'Restaurant not found'}, 404
+        else:
+            return restaurant.to_dict(), 200
+
+    elif request.method == 'DELETE':
+        if not restaurant:
+            return {'error': 'Restaurant not found'}
+        else: 
+            db.session.delete(restaurant)
+            db.session.commit()
+            return {}, 204
+
+@app.route('/pizzas', methods=['GET'])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return [pizza.to_dict(rules=['-restaurant_pizzas']) for pizza in pizzas]
+
+@app.route('/restaurant_pizzas', methods=['GET', 'POST'])
+def create_new_pizza():
+    #line below specific to POST requests. worth memorizing.
+    json_data = request.get_json()
+
+    try: 
+        new_restaurant_pizza = RestaurantPizza(
+            price=json_data.get('price'),
+            pizza_id=json_data.get('pizza_id'),
+            restaurant_id=json_data.get('restaurant_id')
+        )
+    except ValueError as e:
+        return {'errors': ['validation errors']}, 400
+
+    db.session.add(new_restaurant_pizza)
+    db.session.commit()
+    return new_restaurant_pizza.to_dict(), 201
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
@@ -20,9 +21,17 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     address = db.Column(db.String)
 
-    # add relationship
+    # add relationship. First arg in parens is the class name to which you are relating. Second arg is back_populates which points to the relationship attribute in the other class to which you are relating (seems to be lowercase name of the current class...). Third arg is cascading delete if applicable.
+    #cascading delete here is such that when a Restaurant is deleted, all of its restaurant_pizzas are also deleted.
+    restaurant_pizzas = db.relationship(
+        'RestaurantPizza', 
+        back_populates='restaurant', 
+        cascade ='all, delete-orphan'
+    )
 
     # add serialization rules
+    # pattern of note - items inside of the list below are relationshipattribute.back_populates.
+    serialize_rules = ['-restaurant_pizzas.restaurant']
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +45,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza')
 
     # add serialization rules
+    serialize_rules = ['-restaurant_pizzas.pizza']
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -48,12 +59,25 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id')) #foreign keys are tablename.column
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
     # add relationships
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
 
     # add serialization rules
+    serialize_rules = ['-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas']
 
     # add validation
+    #validates(item that is getting validated), third person. self, key, and new_value are params.
+    #validation imported above from sqlalchemy.orm
+    @validates('price')
+    def validates_price(self,key,new_price):
+        if not (1 <= new_price <= 30):
+            raise ValueError('Price must be between 1 and 30.')
+        else:
+            return new_price
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
